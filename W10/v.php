f90d4ca054ea88118e278180d93d8ff43113a5a6
at6333 <at633@njit.edu>
2020-03-27 18:44:21 -0400

    for Prototype 10

--- W10/v.php
+++ W10/v.php
@@ -2,22 +2,23 @@
 session_start();
 header("Content-Type: image/png");
 
-$im = imagecreatetruecolor(400, 400);
-$white = imagecolorallocate($im, 255, 255, 255);
+$im = imagecreatetruecolor(500, 200);
+$fillcolor = imagecolorallocate($im, 0, 175, 200);
 $black = imagecolorallocate($im, 0, 0, 0);
-$font = "LaBelleAurore.ttf"; // must download to CWD
-$text1 = "Good";
-$text2 = "Bye";
-$_SESSION["captcha"] = $text1 . $text2;
+$font1 = "LaBelleAurore.ttf"; // must download to CWD
+$font2 = "Xerox Sans Serif Narrow.ttf";
 
-$text1 = substr(str_shuffle(md5(time() ) ), 0, 4 );
-$text2 = "Bye";
+$text1 = substr(str_shuffle(md5(time() ) ), 0, 2 );
+$text2 = substr(str_shuffle(md5(time() ) ), 0, 2 );
 $_SESSION["captcha"] = $text1 . $text2;
+$sessionid = "Session ID: " . session_id();
+$captcha = "Captcha: " . $_SESSION["captcha"];
 
-imagefilledrectangle($im, 10, 10, 390, 390, $white);
-imagettftext($im, 20, 0, 50, 200, $black, $font, $text1);
-imagettftext($im, 30, -60, 200, 50, $black, $font, $text2);
-imagettftext($im, 30, 0, 20, 380, $black, $font, "stuff");
+imagefilledrectangle($im, 10, 10, 490, 190, $fillcolor);
+imagettftext($im, 20, 30, 150, 75, $black, $font1, $text1);
+imagettftext($im, 30, -60, 300, 125, $black, $font1, $text2);
+imagettftext($im, 10, 0, 20, 170, $black, $font2, $sessionid);
+imagettftext($im, 10, 0, 20, 180, $black, $font2, $captcha);
 
 imagepng($im);
 imagedestroy($im);
